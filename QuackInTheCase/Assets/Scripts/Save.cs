84182129c36e2436355f6a33cd822fcee6a74b42
Maxenec <flick.magic.0o@icloud.com>
2023-10-28 10:22:55 +1100

    Created a dynamic timer function that is reusable every mini game.

---
+++ QuackInTheCase/Assets/Scripts/Save.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+
+public static class Save
+{
+    public static int unlockedEpisode = 1;
+    public static int unlockedGameEp1 = 0;
+}
