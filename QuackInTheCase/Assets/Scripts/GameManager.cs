ee4321c309270f82ad09664cc77e1dbb94264ef4
Personal\Max <flick.magic.0o@icloud.com>
2023-10-30 18:02:42 +1100

    Minor cleanup before pulling to main.

--- QuackInTheCase/Assets/Scripts/GameManager.cs
+++ QuackInTheCase/Assets/Scripts/GameManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
