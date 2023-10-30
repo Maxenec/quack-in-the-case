7e340ef59c9d8ec401b4c5c86a62180371562164
Maxenec <flick.magic.0o@icloud.com>
2023-10-30 13:13:07 +1100

    Made game render in 1920x1080.

--- QuackInTheCase/Assets/Scripts/GameManager.cs
+++ QuackInTheCase/Assets/Scripts/GameManager.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+
+    public TMP_Text resolutionText;
+
+    private void Awake()
+    {
+        
+    }
 
     void Start()
     {
-        
+        int width = Screen.width;
+        int height = Screen.height;
+
+        resolutionText.text = width + " by " + height;
     }
 
     public void SwitchScene(string sceneToLoad)
