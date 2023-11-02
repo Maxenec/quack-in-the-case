08d9051e0fbc18a677395e7bb8e3f34bef51e3d1
Maxenec <flick.magic.0o@icloud.com>
2023-11-02 21:25:51 +1100

    Added car animation with smoke particles after missing the shot, and instructions for what to do.

--- QuackInTheCase/Assets/Scripts/TyreShootout/EnemyCarMovement.cs
+++ QuackInTheCase/Assets/Scripts/TyreShootout/EnemyCarMovement.cs
@@ -8,16 +8,31 @@ public class EnemyCarMovement : MonoBehaviour
     private float rightEdge = 4.5f;
     private float moveSpeed = 6f;
     private bool moveRight;
+    private bool escaped = false;
+
+    private Vector3 initialScale;
+    private float speed = 2f;
+    private float scaleFactor = 5f;
+    private float shrinkDuration = 4;
+    private float timer = 0f;
+
+    public GameObject smoke;
 
     private void Awake()
     {
         moveRight = Random.value > 0.5f;
         SetRandomStartPosition();
+        smoke.SetActive(false);
+    }
+
+    private void Start()
+    {
+        initialScale = transform.localScale;
     }
 
     void SetRandomStartPosition()
     {
-        // Set the initial position of the square to a random x-value within the defined edges
+        //Set the initial position of the square to a random x-value within the defined edges.
         float randomX = Random.Range(leftEdge, rightEdge);
         Vector3 startPosition = new Vector3(randomX, transform.position.y, transform.position.z);
         transform.position = startPosition;
@@ -25,8 +40,15 @@ public class EnemyCarMovement : MonoBehaviour
 
     private void Update()
     {
-        MoveCar();
-        CheckEdges();
+        if (!escaped)
+        {
+            MoveCar();
+            CheckEdges();
+        }
+        else
+        {
+            ZoomAway();
+        }
     }
 
     private void MoveCar()
@@ -52,6 +74,35 @@ public class EnemyCarMovement : MonoBehaviour
         else if (transform.position.x <= leftEdge)
         {
             moveRight = true;
+        }
+    }
+
+    public void ShotMissed()
+    {
+        escaped = true;
+        CarSmoke();
+    }
+
+    private void CarSmoke()
+    {
+        smoke.SetActive(true);
+    }
+
+    private void ZoomAway()
+    {
+        timer += Time.deltaTime;
+
+        if (timer < shrinkDuration)
+        {
+            float progress = timer / shrinkDuration;
+            transform.localScale = Vector3.Lerp(initialScale, initialScale / scaleFactor, progress);
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        }
+        else
+        {
+            transform.localScale = initialScale * scaleFactor;
+            Destroy(gameObject);
         }
     }
 }
