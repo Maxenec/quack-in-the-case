05d6e68a773bd716085e3ec445e68ee6a2ffd0b1
Personal\Max <flick.magic.0o@icloud.com>
2023-11-02 23:59:19 +1100

    Added function to pause game, added a pause menu, and working on save system.

--- QuackInTheCase/Assets/Scripts/GameManager.cs
+++ QuackInTheCase/Assets/Scripts/GameManager.cs
@@ -6,6 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+        {
+            PauseGame();
+            Debug.Log("The game is now paused.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            UnpauseGame();
+            Debug.Log("The game has been resumed.");
+        }
+    }
     public void SwitchScene(string sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
@@ -22,9 +39,10 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    public void SaveGame()
+    public void SaveGame(string level)
     {
-
+        Scene scene = SceneManager.GetActiveScene();
+        PlayerPrefs.SetString("Episode1", scene.name);
     }
 
     public void RetrieveSave()
@@ -32,4 +50,23 @@ public class GameManager : MonoBehaviour
 
     }
 
+    public void PauseGame()
+    {
+        isPaused = true;
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(true);
+        }
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        if (pauseMenu != null)
+        {
+            pauseMenu.SetActive(false);
+        }
+    }
 }
